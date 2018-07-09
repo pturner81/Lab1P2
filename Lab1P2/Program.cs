a3a2a5ea18a82393148e70a0d96919a1439fbfdd
Turner <turnerp81@gmail.com>
2018-07-09 07:30:17 -0400

    Lab1P2

--- Lab1P2/Program.cs
+++ Lab1P2/Program.cs
@@ -10,6 +10,64 @@ namespace Lab1P2
     {
         static void Main(string[] args)
         {
+            {
+
+                DateTime date1, date2;
+
+                int dateDiff, hourDiff, minDiff;
+
+
+
+                Console.WriteLine("Enter Start Date");
+
+                date1 = Convert.ToDateTime(Console.ReadLine());
+
+
+
+                Console.WriteLine("Enter End Date");
+
+                date2 = Convert.ToDateTime(Console.ReadLine());
+
+
+
+                if (date1 < date2)
+
+                {
+
+                    dateDiff = Convert.ToInt32((date2 - date1).TotalDays);
+
+                }
+
+                else
+
+                {
+
+                    dateDiff = Convert.ToInt32((date1 - date2).TotalDays);
+
+                }
+
+                //ensures a positive difference between the dates entered (so chronology isnt needed)
+
+
+
+                hourDiff = dateDiff * 24;
+
+                minDiff = hourDiff * 60;
+
+                // converts the difference in days to hours and minutes, respectively
+
+
+
+                //Console.WriteLine(date1);
+
+                //Console.WriteLine(date2);
+
+                //Console.WriteLine(dateDiff); tests the user input
+
+                Console.WriteLine("The difference between the dates is " + dateDiff + " Days, or " + hourDiff + " Hours, or " + minDiff + " Minutes.");
+
+                Console.ReadKey();
+            }
         }
     }
 }
